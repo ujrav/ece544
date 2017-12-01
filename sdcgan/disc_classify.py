f5dc5d5e449b79bb7e4f8dd1750de1a8cf2d9118
ujrav <ujrav@yahoo.com>
2017-12-01 01:35:47 -0600

    :added initial code

---
+++ sdcgan/disc_classify.py
@@ -0,0 +1,330 @@
+import argparse
+import os
+import random
+
+# library imports
+import numpy as np
+from skimage.color import gray2rgb
+# from skimage.io import imread, imsave
+from skimage.transform import resize
+import tensorflow as tf
+from tensorflow.python.lib.io import file_io
+import scipy.misc
+import numpy as np
+import math
+import cPickle
+
+# local imports
+from models_old import generator, discriminator, discriminator_classify, discriminator_classify_to_labels
+
+
+import pdb
+
+
+# argparse
+parser = argparse.ArgumentParser(description="Use a trained DCGAN Descriminator for Classification")
+parser.add_argument("-c", "--checkpoint-dir", type=str, default="output_manju/checkpoint", help="directory to checkpoints")
+parser.add_argument("-i", "--image-size", type=int, default=64, help="(square) image size")
+parser.add_argument("-s", "--scale-size", type=int, default=64, help="resize length for center crop")
+
+def _read_and_preprocess(paths, scale_len, crop_len):
+    """
+        Reads multiple images (and labels).
+    """
+
+    imgs = []
+
+    for path in paths:
+        
+        file=file_io.FileIO(path,mode='r')
+        img = scipy.misc.imread(file,mode='RGB')
+
+        # force 3-channel images
+        # if img.ndim == 2:
+        #     img = gray2rgb(img)
+        # elif img.shape[2] == 4:
+        #     img = img[:, :, :3]
+
+        # compute the resize dimension
+        resize_f = float(scale_len) / min(img.shape[:2])
+        new_dims = (int(np.round(img.shape[0] * resize_f)),
+                    int(np.round(img.shape[1] * resize_f)))
+
+        # prevent the input image from blowing up
+        # factor of 2 is more or less an arbitrary number
+        max_dim = 2 * scale_len
+        new_dims = (min(new_dims[0], max_dim),
+                    min(new_dims[1], max_dim))
+
+        # resize and center crop
+        img = resize(img, new_dims)
+        top = int(np.ceil((img.shape[0] - crop_len) / 2.0))
+        left = int(np.ceil((img.shape[1] - crop_len) / 2.0))
+        img = img[top:(top+crop_len), left:(left+crop_len)]
+
+        # preprocessing (tanh)
+        img = img*2 - 1
+
+        imgs.append(img)
+
+    return np.array(imgs)
+
+def _read_preprocess_cifar10(cifar_path, cifar_filenames):
+    
+
+    cifar_data = None
+
+    cifar_labels = None
+
+    for filename in cifar_filenames:
+
+        filepath = cifar_path + filename
+
+        file = file_io.FileIO(filepath,mode='r')
+
+        data_dict = cPickle.load(file)
+
+        img_set = data_dict["data"]
+
+        img_set = np.reshape(img_set, (img_set.shape[0],3,32,32))
+
+        img_set = np.swapaxes(np.swapaxes(img_set, 1, 2), 2, 3)
+
+        label_set = data_dict["labels"]
+
+        if cifar_data is None:
+            cifar_data = img_set
+        else:
+            cifar_data = np.concatenate((cifar_data, img_set))
+
+        if cifar_labels is None:
+            cifar_labels = label_set
+        else:
+            cifar_labels = np.concatenate((cifar_labels, label_set))
+
+    cifar_data = cifar_data*2 - 1
+
+    return cifar_data, cifar_labels
+
+def classify_celebA(args):
+
+
+    scale_len = args.scale_size
+    crop_len = args.image_size
+    checkpoint_dir = args.checkpoint_dir
+
+    batch_size = 100
+    n_classes = 3
+    # which attribute columns will we classify
+    class_cols = [1,2,3]
+    train_ratio = 0.8
+    img_set = 5000
+    dataset_path = "data/celebA/"
+
+    img_data = tf.placeholder(tf.float32, shape=[None, 64, 64, 3], name="img_data")
+    label_tensor = tf.placeholder(tf.float32, shape = [None, n_classes], name="label_tensor")
+    is_train = tf.placeholder(tf.bool, name="is_train")
+
+    classifier = discriminator_classify(img_data, n_classes, layer_name="c_classifier_n")
+    classifier_labels = discriminator_classify_to_labels(classifier)
+
+    classification_loss = tf.reduce_mean(tf.reduce_sum((classifier - label_tensor)**2, axis=1))
+
+    d_vars = [var for var in tf.trainable_variables() if "d_" in var.name]
+    c_vars = [var for var in tf.trainable_variables() if "c_" in var.name]
+
+    print d_vars
+    print "______"
+    print c_vars
+
+    opt_classifier = tf.train.AdamOptimizer(0.001, beta1=0.5).minimize(classification_loss, var_list=c_vars)
+
+    #read labels
+    label_data = np.genfromtxt("data/list_attr_celeba.txt",skip_header=2, dtype=None, usecols=class_cols)
+    img_name_data = np.genfromtxt("data/list_attr_celeba.txt",skip_header=2, dtype=None, usecols=(0))
+
+    example_idx = np.arange(0, img_set)
+
+    example_idx_rnd = np.random.permutation(example_idx)
+
+    train_idx = example_idx[0:(int(img_set*0.8))]
+
+    test_idx  = example_idx[(int(img_set*0.8)):]
+
+    num_iter = int(math.ceil(len(train_idx)/batch_size))
+
+
+
+    # saver for discriminator vars only
+    saver = tf.train.Saver(var_list = d_vars)
+
+    sess = tf.Session()
+
+    # intialize tf variables
+    sess.run(tf.global_variables_initializer())
+
+    chkpt_fname = tf.train.latest_checkpoint(checkpoint_dir)
+    saver.restore(sess, chkpt_fname)
+
+    train_idx_shuffle = np.random.permutation(train_idx)
+
+    #train
+    for i in range(0, num_iter):
+        batch_start = i*batch_size
+        batch_end = (i+1)*batch_size
+
+        batch_idx = train_idx_shuffle[batch_start:batch_end]
+
+        batch_labels = label_data[batch_idx, :]
+
+        batch_img_names = img_name_data[batch_idx]
+
+        img_paths = []
+        for img_name in batch_img_names:
+            img_paths.append(dataset_path + img_name)
+
+        batch_imgs = _read_and_preprocess(img_paths, scale_len, crop_len)
+
+        feed_dict_in = {img_data:batch_imgs, label_tensor:batch_labels, is_train:True}
+
+        _, loss = sess.run([opt_classifier, classification_loss], feed_dict = feed_dict_in)
+
+        print loss
+
+    #test
+    test_labels = label_data[test_idx,:]
+
+    test_img_names = img_name_data[test_idx]
+
+    img_paths = []
+    for img_name in test_img_names:
+            img_paths.append(dataset_path + img_name)
+
+    test_imgs = _read_and_preprocess(img_paths, scale_len, crop_len)
+
+    feed_dict_test = {img_data:test_imgs, label_tensor:test_labels, is_train:True}
+
+    test_loss, test_label_out = sess.run([classification_loss, classifier_labels], feed_dict = feed_dict_test)
+
+    test_acc = np.mean(test_labels == test_label_out, axis = 0)
+
+    print test_acc
+
+    pdb.set_trace()
+
+def classify_cifar(args):
+
+
+    scale_len = args.scale_size
+    crop_len = args.image_size
+    checkpoint_dir = args.checkpoint_dir
+
+    batch_size = 100
+    n_classes = 3
+    # which attribute columns will we classify
+    class_cols = [1,2,3]
+    train_ratio = 0.8
+    img_set = 5000
+    dataset_path = "data/celebA/"
+
+    img_data = tf.placeholder(tf.float32, shape=[None, 64, 64, 3], name="img_data")
+    label_tensor = tf.placeholder(tf.float32, shape = [None, n_classes], name="label_tensor")
+    is_train = tf.placeholder(tf.bool, name="is_train")
+
+    classifier = discriminator_classify(img_data, n_classes, layer_name="c_classifier_n")
+    classifier_labels = discriminator_classify_to_labels(classifier)
+
+    classification_loss = tf.reduce_mean(tf.reduce_sum((classifier - label_tensor)**2, axis=1))
+
+    d_vars = [var for var in tf.trainable_variables() if "d_" in var.name]
+    c_vars = [var for var in tf.trainable_variables() if "c_" in var.name]
+
+    print d_vars
+    print "______"
+    print c_vars
+
+    opt_classifier = tf.train.AdamOptimizer(0.001, beta1=0.5).minimize(classification_loss, var_list=c_vars)
+
+    #read labels
+    label_data = np.genfromtxt("data/list_attr_celeba.txt",skip_header=2, dtype=None, usecols=class_cols)
+    img_name_data = np.genfromtxt("data/list_attr_celeba.txt",skip_header=2, dtype=None, usecols=(0))
+
+    example_idx = np.arange(0, img_set)
+
+    example_idx_rnd = np.random.permutation(example_idx)
+
+    train_idx = example_idx[0:(int(img_set*0.8))]
+
+    test_idx  = example_idx[(int(img_set*0.8)):]
+
+    num_iter = int(math.ceil(len(train_idx)/batch_size))
+
+
+
+    # saver for discriminator vars only
+    saver = tf.train.Saver(var_list = d_vars)
+
+    sess = tf.Session()
+
+    # intialize tf variables
+    sess.run(tf.global_variables_initializer())
+
+    chkpt_fname = tf.train.latest_checkpoint(checkpoint_dir)
+    saver.restore(sess, chkpt_fname)
+
+    train_idx_shuffle = np.random.permutation(train_idx)
+
+    #train
+    for i in range(0, num_iter):
+        batch_start = i*batch_size
+        batch_end = (i+1)*batch_size
+
+        batch_idx = train_idx_shuffle[batch_start:batch_end]
+
+        batch_labels = label_data[batch_idx, :]
+
+        batch_img_names = img_name_data[batch_idx]
+
+        img_paths = []
+        for img_name in batch_img_names:
+            img_paths.append(dataset_path + img_name)
+
+        batch_imgs = _read_and_preprocess(img_paths, scale_len, crop_len)
+
+        feed_dict_in = {img_data:batch_imgs, label_tensor:batch_labels, is_train:True}
+
+        _, loss = sess.run([opt_classifier, classification_loss], feed_dict = feed_dict_in)
+
+        print loss
+
+    #test
+    test_labels = label_data[test_idx,:]
+
+    test_img_names = img_name_data[test_idx]
+
+    img_paths = []
+    for img_name in test_img_names:
+            img_paths.append(dataset_path + img_name)
+
+    test_imgs = _read_and_preprocess(img_paths, scale_len, crop_len)
+
+    feed_dict_test = {img_data:test_imgs, label_tensor:test_labels, is_train:True}
+
+    test_loss, test_label_out = sess.run([classification_loss, classifier_labels], feed_dict = feed_dict_test)
+
+    test_acc = np.mean(test_labels == test_label_out, axis = 0)
+
+    print test_acc
+
+    pdb.set_trace()
+
+def main(args):
+    #classify_celebA(args)
+    cifar_batch_files = ["data_batch_1", "data_batch_2", "data_batch_3", "data_batch_4", "data_batch_5"]
+    cifar_data, cifar_labels = read_cifar10("data/cifar_10/", cifar_batch_files)
+    pdb.set_trace()
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
---
+++ sdcgan/models_old.py
@@ -0,0 +1,254 @@
+"""
+models.py: Definitions for generator and discriminator convnets.
+"""
+
+import tensorflow as tf
+
+
+# commonly used stride settings for 2D convolutions
+STRIDE_1 = [1, 1, 1, 1]
+STRIDE_2 = [1, 2, 2, 1]
+
+
+#Manju: commented the following function for batch normalization that this original guy actually wrote manually.
+#I did so because the new tensorflow has a built-in function that does better than the following hard coded one. 
+
+
+# def _bn(bottom, is_train):
+#     """
+#         Creates a batch normalization op.
+#         Meant to be invoked from other layer operations.
+#     """
+
+#     # assume inference by default
+    
+
+#     # create scale and shift variables
+#     bn_shape = bottom.get_shape()[-1]
+#     shift = tf.get_variable("beta", shape=bn_shape,
+#                             initializer=tf.constant_initializer(0.0))
+#     scale = tf.get_variable("gamma", shape=bn_shape,
+#                             initializer=tf.constant_initializer(1.0))
+
+#     # compute mean and variance
+#     bn_axes = list(range(len(bottom.get_shape()) - 1))
+#     (mu, var) = tf.nn.moments(bottom, bn_axes)
+
+#     # batch normalization ops
+#     with tf.variable_scope(tf.get_variable_scope(), reuse=False):
+#         ema = tf.train.ExponentialMovingAverage(decay=0.99)
+
+#     def train_op():
+#         with tf.variable_scope(tf.get_variable_scope(), reuse=False):
+#             ema_op = ema.apply([mu, var])
+#         with tf.control_dependencies([ema_op]):
+#             return (tf.identity(mu), tf.identity(var))
+
+#     def test_op():
+#         moving_mu = ema.average(mu)
+#         moving_var = ema.average(var)
+#         return (moving_mu, moving_var)
+
+#     (mean, variance) = tf.cond(is_train, train_op, test_op)
+
+#     top = tf.nn.batch_normalization(bottom, mean, variance, shift, scale, 1e-4)
+
+#     return top
+
+
+def conv2d(name, bottom, shape, strides, top_shape=None, with_bn=True, is_train=None):
+    """
+        Creates a convolution + BN block.
+    """
+    
+
+    with tf.variable_scope(name) as scope:
+        
+        # apply batch normalization, if necessary
+        # Manju: The new batch normalization function is built-in:
+        bn = tf.contrib.layers.batch_norm(bottom, is_training=is_train) if with_bn else bottom
+        # add convolution op
+        weights = tf.get_variable("weights", shape=shape,
+                                  initializer=tf.truncated_normal_initializer(stddev=0.02))
+        if top_shape is not None:
+            conv = tf.nn.conv2d_transpose(bn, weights, top_shape, strides, padding="SAME")
+        else:
+            conv = tf.nn.conv2d(bn, weights, strides, padding="SAME")
+
+        # add biases
+        bias_shape = [shape[-1]] if top_shape is None else [shape[-2]]
+        biases = tf.get_variable("biases", shape=bias_shape,
+                                 initializer=tf.constant_initializer())
+        top = tf.nn.bias_add(conv, biases)
+
+    return top
+
+
+def linear(name, bottom, shape, with_bn=True, is_train=None):
+    """
+        Creates a fully connected + BN block.
+    """
+    
+
+    with tf.variable_scope(name) as scope:
+
+        # apply batch normalization, if necessary
+        bn = tf.contrib.layers.batch_norm(bottom, is_training=is_train) if with_bn else bottom
+        # inner product
+        weights = tf.get_variable("weights", shape=shape,
+                                  initializer=tf.truncated_normal_initializer(stddev=0.02))
+        linear = tf.matmul(bn, weights)
+
+        # add biases
+        bias_shape = [shape[-1]]
+        biases = tf.get_variable("biases", shape=bias_shape,
+                                 initializer=tf.constant_initializer())
+        top = tf.nn.bias_add(linear, biases)
+
+    return top
+
+
+def generator(data, is_train, side_length):
+    """
+        Builds the generator model.
+
+        :param tensorflow.Tensor data:
+            A 4-D input tensor with the last dimension Gaussian sampled.
+
+        :param tensorflow.Tensor is_train:
+            Determines whether or not the generator is used only for inference.
+
+        :param int side_length:
+            Image side length.
+    """
+
+    
+
+    assert side_length % 16 == 0, "image side length must be divisible by 16"
+
+    dim = side_length // 16
+    (batch_size, z_len) = data.get_shape().as_list()
+
+    # project block, 1024 outputs
+    proj = tf.nn.relu(linear("g_proj", data, [z_len, dim * dim * 1024], with_bn=False))
+    rshp = tf.reshape(proj, [batch_size, dim, dim, 1024])
+
+    # conv1 block, 512 outputs
+    dim *= 2
+    conv1 = tf.nn.relu(conv2d("g_conv1", rshp, [5, 5, 512, 1024], STRIDE_2,
+                              top_shape=[batch_size, dim, dim, 512], is_train=is_train))
+
+    # conv2 block, 256 outputs
+    dim *= 2
+    conv2 = tf.nn.relu(conv2d("g_conv2", conv1, [5, 5, 256, 512], STRIDE_2,
+                              top_shape=[batch_size, dim, dim, 256], is_train=is_train))
+
+    # conv3 block, 128 outputs
+    dim *= 2
+    conv3 = tf.nn.relu(conv2d("g_conv3", conv2, [5, 5, 128, 256], STRIDE_2,
+                              top_shape=[batch_size, dim, dim, 128], is_train=is_train))
+
+    # conv4 block, 64 outputs
+    dim *= 2
+    conv4 = tf.nn.relu(conv2d("g_conv4", conv3, [5, 5, 64, 128], STRIDE_2,
+                              top_shape=[batch_size, dim, dim, 64], is_train=is_train))
+
+    # conv5 block, 3 outputs
+    conv5 = tf.nn.tanh(conv2d("g_conv5", conv4, [3, 3, 3, 64], STRIDE_1,
+                              top_shape=[batch_size, dim, dim, 3], is_train=is_train))
+
+    top = conv5
+
+    return top
+
+
+def discriminator(data, is_train,reuse=False):
+    """
+        Builds the discriminator network.
+
+        :param tensorflow.Tensor data:
+            A 4-D tensor representing an input image.
+
+        :param tensorflow.Tensor is_train:
+            Determines whether or not the discriminator is used only for inference.
+    """
+
+    # conv1 block, 128 outputs
+    conv1 = tf.nn.relu(conv2d("d_conv1", data, [5, 5, 3, 128], STRIDE_2, with_bn=False))
+
+    # conv2 block, 256 outputs
+    conv2 = tf.nn.relu(conv2d("d_conv2", conv1, [5, 5, 128, 256], STRIDE_2, is_train=is_train))
+
+    # conv3 block, 512 outputs
+    conv3 = tf.nn.relu(conv2d("d_conv3", conv2, [5, 5, 256, 512], STRIDE_2, is_train=is_train))
+
+    # conv4 block, 1024 outputs
+    conv4 = tf.nn.relu(conv2d("d_conv4", conv3, [5, 5, 512, 1024], STRIDE_2, is_train=is_train))
+
+    # average pooling
+    avg_pool = tf.reduce_mean(conv4, [1, 2])
+
+    # fully connected
+    classifier = linear("d_classifier", avg_pool, [1024, 1], with_bn=False)
+
+    top = classifier
+
+    return top
+
+def discriminator_features(data, is_train,reuse=False):
+    """
+        Builds the discriminator network.
+
+        :param tensorflow.Tensor data:
+            A 4-D tensor representing an input image.
+
+        :param tensorflow.Tensor is_train:
+            Determines whether or not the discriminator is used only for inference.
+    """
+
+    # conv1 block, 128 outputs
+    conv1 = tf.nn.relu(conv2d("d_conv1", data, [5, 5, 3, 128], STRIDE_2, with_bn=False))
+
+    # conv2 block, 256 outputs
+    conv2 = tf.nn.relu(conv2d("d_conv2", conv1, [5, 5, 128, 256], STRIDE_2, is_train=is_train))
+
+    # conv3 block, 512 outputs
+    conv3 = tf.nn.relu(conv2d("d_conv3", conv2, [5, 5, 256, 512], STRIDE_2, is_train=is_train))
+
+    # conv4 block, 1024 outputs
+    conv4 = tf.nn.relu(conv2d("d_conv4", conv3, [5, 5, 512, 1024], STRIDE_2, is_train=is_train))
+
+    # average pooling
+    avg_pool = tf.reduce_mean(conv4, [1, 2])
+
+    top = avg_pool
+
+    return top
+
+def discriminator_classify(data, n_classes, feature_size = 1024, layer_name="c_classifier_n"):
+
+    features = discriminator_features(data, is_train = False, reuse = True)
+
+    classifier = linear(layer_name, features, [feature_size, n_classes], with_bn = False)
+
+    return classifier
+
+def discriminator_classify_to_labels(classification_scores):
+
+    output_classes = tf.where(classification_scores >= 0, tf.ones_like(classification_scores), -1*tf.ones_like(classification_scores))
+
+    return output_classes
+
+def discriminator_classify_exclusive(data, n_classes, feature_size = 1024, layer_name="c_classifier_n"):
+
+    features = discriminator_features(data, is_train = False, reuse = True)
+
+    classifier = linear(layer_name, features, [feature_size, n_classes], with_bn = False)
+
+    return classifier
+
+def discriminator_classify_to_labels_exclusive(classification_scores):
+
+    output_classes = tf.argmax(classification_scores, axis = 1)
+
+    return output_classes
